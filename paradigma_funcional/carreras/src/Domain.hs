d8bd92a3cf455f24f429868826584f345cf8b8a4
Joan Canossa <jfcpersonal99@gmail.com>
2023-08-21 18:31:19 -0300

    Carreras Terminado

--- paradigma_funcional/carreras/src/Domain.hs
+++ paradigma_funcional/carreras/src/Domain.hs
@@ -3,12 +3,17 @@ import PdePreludat
 import Library
 
 autoA :: Auto
-autoA = Auto "verde" 200 100 0
+autoA = Auto Rojo 120 0
 
 autoB :: Auto
-autoB = Auto "rojo" 200 105 0
+autoB = Auto Blanco 120 0
 
 autoC :: Auto
-autoC = Auto "gris" 200 80 0
+autoC = Auto Azul 120 0
 
-carrera1 = [autoA,autoB,autoC]
+autoD :: Auto
+autoD = Auto Negro 120 0
+
+carrera1 = [autoA,autoB,autoC,autoD]
+
+eventos = [correnTodos 30,jetpack (Just 3) Azul,terremoto Nothing Blanco, correnTodos 40,miguelitos (Just 20) Blanco,jetpack (Just 6) Negro, correnTodos 10] -- devuelve una lista de [Carrera]
--- paradigma_funcional/carreras/src/Library.hs
+++ paradigma_funcional/carreras/src/Library.hs
@@ -1,11 +1,13 @@
 module Library where
 import PdePreludat
 
+data Color = Azul | Rojo | Blanco | Negro
+    deriving (Show, Eq, Ord)
+
 data Auto = Auto{
-    color::String,
+    color::Color,
     velocidad::Number,
-    distancia::Number,
-    posicion::Number
+    distancia::Number
 }deriving(Show, Eq)
 
 type Carrera = [Auto]
@@ -15,7 +17,6 @@ autosDistintos auto1 auto2 = color auto1 /= color auto2
 
 mismoAuto :: Auto -> Auto -> Bool
 mismoAuto auto1 auto2 = color auto1 == color auto2
-
 
 estaCerca :: Auto -> Auto -> Bool
 estaCerca auto1 auto2 = autosDistintos auto1 auto2 && ((<10).distanciaEntreAutos auto1) auto2
@@ -55,11 +56,37 @@ afectarALosQueCumplen :: (a -> Bool) -> (a -> a) -> [a] -> [a]
 afectarALosQueCumplen criterio efecto lista
     = (map efecto . filter criterio) lista ++ filter (not.criterio) lista
 
-terremoto :: Auto -> Carrera -> Carrera
-terremoto auto carrera = afectarALosQueCumplen (estaCerca auto) (bajarVelocidad 50) carrera
+type PowerUp = Maybe Number -> Color -> Carrera -> Carrera
+terremoto :: PowerUp
+terremoto (Nothing) color carrera = afectarALosQueCumplen (estaCerca (getAutoByColor color carrera)) (bajarVelocidad 50) carrera
 
-miguelitos :: Number -> Auto -> Carrera -> Carrera
-miguelitos valor auto carrera = afectarALosQueCumplen (vaGanando auto) (bajarVelocidad valor) carrera
+miguelitos :: PowerUp
+miguelitos (Just valor) color carrera = afectarALosQueCumplen (vaGanando (getAutoByColor color carrera)) (bajarVelocidad valor) carrera
 
-jetpack :: Number -> Auto -> Carrera -> Carrera
-jetpack tiempo auto carrera = afectarALosQueCumplen (mismoAuto auto) ((corra tiempo).(alterarVelocidad (*2))) carrera
+jetpack :: PowerUp
+jetpack (Just tiempo) color carrera = afectarALosQueCumplen (mismoAuto (getAutoByColor color carrera)) ((corra tiempo).(alterarVelocidad (*2))) carrera
+
+getAutoByColor :: Color -> Carrera -> Auto
+getAutoByColor colorId carrera = head(filter ((==colorId).color) carrera)
+
+simularCarrera :: Carrera -> [Carrera -> Carrera] -> [(Number, Color)]
+simularCarrera carrera eventos = getEstadoFinal (foldr ($) carrera (reverse eventos))
+
+correnTodos :: Number -> Carrera -> Carrera
+correnTodos tiempo carrera = map (corra tiempo) carrera
+
+usaPowerUp :: (PowerUp) -> (PowerUp)
+usaPowerUp powerup = powerup
+
+getEstadoFinal :: Carrera -> [(Number, Color)]
+getEstadoFinal carrera = quicksort (map (flip estadoFinal carrera) carrera)
+
+estadoFinal :: Auto -> Carrera -> (Number, Color)
+estadoFinal auto carrera = (puesto auto carrera,color auto)
+
+quicksort :: [(Number, a)] -> [(Number, a)]
+quicksort [] = []
+quicksort ((x, y):xs) =
+    let lesser = quicksort [(a, b) | (a, b) <- xs, a <= x]
+        greater = quicksort [(a, b) | (a, b) <- xs, a > x]
+    in lesser ++ [(x, y)] ++ greater
